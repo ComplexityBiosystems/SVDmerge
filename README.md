54797fb099dbf9c80b3691a24fc3036b75c2dda1
Francesc Font Clos <francesc.font@gmail.com>
2017-01-02 22:25:02 +0100

    starting to write tutorial

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# TwoStepFilter
-A python module implementing the Two Step SVD Filter as descrived in [insert publication someday]().
+# SVDmerge
+A python module implementing the two-step SVD merging method, as descrived in [insert publication someday]().
 
-Currently **NOT STABLE**. The *obesity score* projet has his own dedicated stable pipeline.
 
 
--- SVDmerge.py
+++ SVDmerge.py
@@ -9,7 +9,7 @@ import seaborn as sns
 
 def check_data_integrity(geno=None,pheno=None,column=None,verbose=False,plot_only=False):
 
-    # check if all data was supplied
+    # Check if all data was supplied
     if geno is None:
         raise NameError("You must supply the expression matrix as a pandas Dataframe") 
     
@@ -19,44 +19,42 @@ def check_data_integrity(geno=None,pheno=None,column=None,verbose=False,plot_onl
     if column is None:
         raise NameError("You must indicate which column of pheno sohuld be used to differentiate samples") 
     
-    # check if indices of geno and pheno are the same
+    # Check that indices of geno and pheno coincide
     if set(geno.index)!=set(pheno.index) or geno.shape[0]!=pheno.shape[0]:
         raise NameError("Indices of geno and pheno must coincide")
 
-    # check unique labels in indices
+    # Check unique labels in indices
     if max(Counter(geno.index).values())>1:
         if verbose:print "Warning: there are non-unique labels in geno index"
     if max(Counter(pheno.index).values())>1:
         if verbose:print "Warning: there are non-unique labels in pheno index"
     
-    # check if column in phenotype is good
+    # Check that column exists in pheno df
     if column not in pheno.columns:        
         raise NameError("%s not found in pheno dataframe"%column) 
     
+    # If not plotting, check that column has only two unique labels
     if plot_only is False:
         if np.unique(pheno[column]).shape[0]!=2:
             raise NameError("Column %s in pheno must contain exactly two unique labels")
 
 
 
-def simple_filter(geno=None,pheno=None,column=None,verbose=False,extra_kill=False,kill_all=False):
+def onestep_filter(geno=None,pheno=None,column=None,verbose=False):
     """
-    Perform a simple SVD filtering step.
+    Performs a single SVD filtering step.
     geno: expression matrix
     pheno: clinical data
-    colum: column of pheno to be used to differentiate
-    extra_kill: (experimental) kills also the eigenvect to the right of the chosen one
-    kill_all: (experimental) kill all except chosen one
+    colum: column of pheno to be used to define groups
     """
 
-    ### CHECKS
+    # Check data integrity
     check_data_integrity(geno=geno,pheno=pheno,column=column,verbose=verbose)    
 
-
-    # create groups
+    # Create groups
     lab1,lab2 = np.unique(pheno[column].values)
 
-    # create pca dataframe
+    # Create PCA dataframe
     pca = PCA(whiten=False).fit(geno)
     geno_pca = pd.DataFrame(
         data = pca.transform(geno),
@@ -64,9 +62,7 @@ def simple_filter(geno=None,pheno=None,column=None,verbose=False,extra_kill=Fals
         columns = ["pca"+str(i) for i in range(min(geno.shape))],
         )
     
-
-
-    # compute KS values
+    # Compute Kolmogorov-Smirnov tests
     pvals =  ([ks_2samp(
         geno_pca.loc[pheno[column]==lab1,pca_col],
         geno_pca.loc[pheno[column]==lab2,pca_col]
@@ -83,13 +79,8 @@ def simple_filter(geno=None,pheno=None,column=None,verbose=False,extra_kill=Fals
     # set first min_pval_col components to zero
     geno_pca.iloc[:,:min_pval_col] = 0
 
-    if extra_kill:
-        geno_pca.iloc[:,min_pval_col+1] = 0
-
-    if kill_all:
-        geno_pca.iloc[:,min_pval_col+1:] = 0
-
-
+    
+    # return values in dataframe
     return pd.DataFrame(
         data = pca.inverse_transform(geno_pca.values),
         index = geno.index,
@@ -132,24 +123,36 @@ def plot_pca_2d(geno=None,pheno=None,column=None,figsize=(6,5),x_axis="pca0",y_a
     plt.ylabel(y_axis.upper(),fontsize=14)
 
 def pca_df(df):
+    """
+    I AM NOT USING THIS FUNCTION AT ALL ...
+    """
     return pd.DataFrame(
         data = PCA(whiten=True).fit_transform(df),
         index = df.index,
         columns = ["pca"+str(i) for i in range(min(df.shape))]
     )
 
-def two_step_filter(geno_list=None,pheno_list=None,column=None,verbose=False,extra_kill = False,kill_all = False):
+def twostep_filter(geno_list=None,pheno_list=None,column=None,verbose=False):
+    """
 
-    # first filter batch by batch
+    Merges a set of datasets, removing batch effects via the two-step SVD merging method.
+
+    geno_list: list of dataframes containing gene-expression values (each item corresponds to a batch).
+    pheno_list: associated list of dataframes containing clinical data (ordered as in geno_list).
+    column: name of a column in pheno_list to be used to define two groups (usually healthy/disease).
+
+    
+    """
+    # First filter batch by batch
     genos_1f = []
     for geno,pheno in zip(geno_list,pheno_list):
-        g =simple_filter(geno=geno,pheno=pheno,column=column,verbose=verbose,extra_kill=extra_kill,kill_all = kill_all)
+        g =onestep_filter(geno=geno,pheno=pheno,column=column,verbose=verbose,extra_kill=extra_kill,kill_all = kill_all)
         genos_1f.append(g)
 
     # merge batches
     geno_1f = pd.concat(genos_1f,join="inner")
     pheno = pd.concat(pheno_list,join="inner")
 
-    return simple_filter( geno = geno_1f , pheno = pheno , column = column , verbose = verbose , extra_kill = extra_kill, kill_all = False)
+    return onestep_filter( geno = geno_1f , pheno = pheno , column = column , verbose = verbose , extra_kill = extra_kill, kill_all = False)
 
 
